e212719d647ad650710617072f34f09a122121a6
Alhaimouni <m.alhaumouni@gmail.com>
2022-07-10 22:28:05 +0300

    no flex yet

--- app.js
+++ app.js
@@ -3,12 +3,14 @@
 const allEmployees=[];
 
 
-function Employee(id,name,dep,level)
+function Employee(id,name,dep,level,image)
 {
+
 this.id=id;
 this.name=name;
 this.department=dep;
 this.level=level;
+this.image=image;
 this.salary=this.netSalary();
 allEmployees.push(this);
 }
@@ -27,96 +29,64 @@ Employee.prototype.netSalary = function()
     return this.salaryWithoutTax()*0.925;
 }
 
-let E1 = new Employee(1000,"Ghazi Samer","Administration","Senior");
-let E2 = new Employee(1001,"Lana Ali","Finance","Senior");
-let E3 = new Employee(1002,"Tamara Ayoub","Markiting","Senior");
-let E4 = new Employee(1003,"Safi Walid","Administration","Mid-Senior");
-let E5 = new Employee(1004,"Omar Zaid","Development","Senior");
-let E6 = new Employee(1005,"Rana Saleh","Development","Junior");
-let E7 = new Employee(1006,"Hadi Ahmad","Finance","Mid-Senior"); 
+let E1 = new Employee(1000,"Ghazi Samer","Administration","Senior","./assets/Ghazi.jpg");
+let E2 = new Employee(1001,"Lana Ali","Finance","Senior","./assets/Lana.jpg");
+let E3 = new Employee(1002,"Tamara Ayoub","Markiting","Senior","./assets/Tamara.jpg");
+let E4 = new Employee(1003,"Safi Walid","Administration","Mid-Senior","./assets/Safi.jpg");
+let E5 = new Employee(1004,"Omar Zaid","Development","Senior","./assets/Omar.jpg");
+let E6 = new Employee(1005,"Rana Saleh","Development","Junior","./assets/Rana.jpg");
+let E7 = new Employee(1006,"Hadi Ahmad","Finance","Mid-Senior","./assets/Hadi.jpg"); 
 
-
-for(let i=0 ;i<allEmployees.length;i++)
+Employee.prototype.employeeData = function()
 {
+    let par = document.createElement("p");
+    par.textContent = "Name:"+this.name+"-ID :"+this.id+"-Department :"+this.department+"-Level:"+this.level;
 
-document.write("Employee name   : "+ allEmployees[i].name + "<br>" +"Salary   : "+ allEmployees[i].salary+"JD");
-document.write("<hr></hr>");
+    //=========================================================================================================//
+
+    let addImgElement = document.createElement("img");
+    addImgElement.src = this.image;
+    addImgElement.alt = "Employee Image";
+    addImgElement.style.cssText ="width:20%; hieght:15%";
+
+    //========================================================================================================//
+
+    let divPlace;
+    switch(this.department)
+    {
+        case "Administration" :
+            divPlace = document.getElementById("adm");
+            break;
+        case "Development" :
+            divPlace = document.getElementById("dev");
+            break;
+        case "Markiting" :
+            divPlace = document.getElementById("mar");
+            break;
+        case "Finance" :
+            divPlace = document.getElementById("fin");
+            break;
+    }
+
+    
+    divPlace.appendChild(addImgElement);
+    divPlace.appendChild(par);
 }
 
+let divEl = document.getElementsByClassName("od");
+divEl[0].style.cssText ="color:darkblue; font-style: oblique ; border-style: groove ; text-align : left ; border-color : gray";
+divEl[1].style.cssText ="color:darkblue; font-style: oblique ; border-style: groove ; text-align : left; border-color : gray";
+divEl[2].style.cssText ="color:darkblue; font-style: oblique ; border-style: groove ; text-align : left; border-color : gray";
+divEl[3].style.cssText ="color:darkblue; font-style: oblique ; border-style: groove ; text-align : left; border-color : gray";
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//============================================ Starting ============================================//
-
-
-/*const allEmp =[];
-
-function Employee(id,name,dep,level)
+function render()
 {
-
-this.employeeid = id;
-this.name = name;
-this.department = dep;
-this.level = level;
-this.salary = this.calSalary();
-
-allEmp.push(this);
+    for (let i=0;i<allEmployees.length;i++)
+    {   
+        allEmployees[i].employeeData();
+    }
 }
 
-Employee.prototype.calSalary = function()
-{
-    if (this.level==="Junior") this.salary= Math.floor(Math.random()*(1000-500+1))+500;
-    if (this.level==="Mid-Senior") this.salary= Math.floor(Math.random()*(1500-1000+1))+1000;
-    if (this.level==="Senior") this.salary= Math.floor(Math.random()*(2000-1500+1))+1500;
-
-    return this.salary*0.925;
-}
-
-let E1 = new Employee(1000,"Ghazi Samer","Administration","Senior");
-let E2 = new Employee(1001,"Lana Ali","Finance","Senior");
-let E3 = new Employee(1002,"Tamara Ayoub","Markiting","Senior");
-let E4 = new Employee(1003,"Safi Walid","Administration","Mid-Senior");
-let E5 = new Employee(1004,"Omar Zaid","Development","Senior");
-let E6 = new Employee(1005,"Rana Saleh","Development","Junior");
-let E7 = new Employee(1006,"Hadi Ahmad","Finance","Mid-Senior");
-
-
-for(let i=0 ;i<allEmp.length;i++)
-{
-console.log(allEmp[i]);
-}
-*/
+render();
