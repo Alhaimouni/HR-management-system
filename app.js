801c9a12221221f34ef038b554829420bd25f12d
Alhaimouni <m.alhaumouni@gmail.com>
2022-07-11 13:30:07 +0300

    adding cards

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ Employee.prototype.salaryWithoutTax= function()
 
 Employee.prototype.netSalary = function()
 {
-    return this.salaryWithoutTax()*0.925;
+    return Math.floor(this.salaryWithoutTax()*0.925);
 }
 
 let E1 = new Employee(1000,"Ghazi Samer","Administration","Senior","./assets/Ghazi.jpg");
@@ -36,20 +36,17 @@ let E4 = new Employee(1003,"Safi Walid","Administration","Mid-Senior","./assets/
 let E5 = new Employee(1004,"Omar Zaid","Development","Senior","./assets/Omar.jpg");
 let E6 = new Employee(1005,"Rana Saleh","Development","Junior","./assets/Rana.jpg");
 let E7 = new Employee(1006,"Hadi Ahmad","Finance","Mid-Senior","./assets/Hadi.jpg"); 
+let E8 = new Employee(666,"Mohammad jihad","Development","Junior","./assets/12096.jpg");
 
 Employee.prototype.employeeData = function()
 {
     let par = document.createElement("p");
-    par.textContent = "Name:"+this.name+"-ID :"+this.id+"-Department :"+this.department+"-Level:"+this.level;
-
-    //=========================================================================================================//
+    par.textContent = "Name: "+this.name+" ID :"+this.id+" Level: "+this.level+"  Salary : " +this.salary+ " JD";
 
     let addImgElement = document.createElement("img");
     addImgElement.src = this.image;
     addImgElement.alt = "Employee Image";
     addImgElement.style.cssText ="width:20%; hieght:15%";
-
-    //========================================================================================================//
 
     let divPlace;
     switch(this.department)
@@ -67,19 +64,35 @@ Employee.prototype.employeeData = function()
             divPlace = document.getElementById("fin");
             break;
     }
+   
+    //=========================================================== With  Table ===========================================================//
 
-    
-    divPlace.appendChild(addImgElement);
-    divPlace.appendChild(par);
-}
+    let card = document.createElement("table");
+    let pic = document.createElement("tr");
+    let dat = document.createElement("tr");
+    let space = document.createElement("tr");
+    let br = document.createElement("br");
+
+    space.appendChild(br);
+    pic.appendChild(addImgElement);
+    dat.appendChild(par);
+    card.appendChild(pic);
+    card.appendChild(dat);
+    card.appendChild(space);
+    divPlace.appendChild(card);
+    pic.setAttribute("class","cd");
+    dat.setAttribute("class","cdd");
+    //========================================================== With Out Table ===========================================================//
+
+    /* divPlace.appendChild(addImgElement);
+    divPlace.appendChild(par);  */
+};
 
 let divEl = document.getElementsByClassName("od");
 divEl[0].style.cssText ="color:darkblue; font-style: oblique ; border-style: groove ; text-align : left ; border-color : gray";
 divEl[1].style.cssText ="color:darkblue; font-style: oblique ; border-style: groove ; text-align : left; border-color : gray";
 divEl[2].style.cssText ="color:darkblue; font-style: oblique ; border-style: groove ; text-align : left; border-color : gray";
 divEl[3].style.cssText ="color:darkblue; font-style: oblique ; border-style: groove ; text-align : left; border-color : gray";
-
-
 
 function render()
 {
